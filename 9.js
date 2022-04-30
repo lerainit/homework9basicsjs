f69e2ca8a2b030b0115b43b3c83b02bd7ec3e939
Valeria Vitvytska <leranmu@gmail.com>
2022-04-30 08:28:01 +0300

    9commit

---
+++ 9.js
@@ -0,0 +1,102 @@
+
+let citiesSimple = ['kyiv', 'kharkiv', 'dnipro', 'vinnytsa', 'lviv', 'rivne'];
+
+/* Первый вариант решения для простых массивов*/
+
+function createList(array) {
+
+  let newArray = array.map(item => `<li>${item}</li>`);
+
+
+  return newArray.join(' ');
+
+}
+
+
+function insertList(position,str, elem = document.body ) {
+
+  if (elem === document.body) {
+
+
+    return elem.insertAdjacentHTML(position, str)
+
+  }
+
+
+  let elem1 = document.createElement(elem);
+
+  document.body.append(elem1);
+
+
+  return elem1.insertAdjacentHTML(position, str);
+
+}
+
+insertList('afterbegin',createList(citiesSimple),'ul');
+
+/*--------------------------------------------------------------------------------------*/
+
+/* Второй вариант решения для простых массивов */
+
+
+
+function listCreate(array,elem = document.body) {
+
+
+  let newArray = array.map(item => `<li>${item}</li>`)
+
+
+  if (elem === document.body) {
+
+    return newArray.join(' ');
+  }
+
+
+  let newList = newArray.join(' ');
+
+
+  return `<${elem}>${newList}</${elem}>`
+
+
+}
+
+
+function listInsert(position, str) {
+
+
+  return document.body.insertAdjacentHTML(position, str);
+
+}
+
+listInsert('beforeend', listCreate(citiesSimple,'ul'));
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/* Задание повышенной сложности */
+
+/* Вариант для массивов с вложенными масивами--------*/
+
+let cities = ["Rivne", ["Kharkiv", "lviv"], "Kiev", ["Borispol", "Irpin"], "Odessa", ["Lviv","Cherihiv","Uzhhorod"], "Dnieper"];
+
+
+function transformArray(array, elchild, elem = document.body) {
+
+
+  let newArr = array.map(item => Array.isArray(item) ? `<${elchild}>${transformArray(item, elchild, elem)}</${elchild}>` : `<${elchild}>${item}</${elchild}>`);
+
+
+  if (elem === document.body) { return newArr.join(' ') }
+
+
+  let newStr = newArr.join(' ');
+
+
+  return `<${elem}>${newStr}</${elem}>`;
+
+}
+
+const insertTransformedArray = (position, str) => document.body.insertAdjacentHTML(position, str)
+
+
+
+insertTransformedArray('afterbegin', transformArray(cities, 'li','ul'))
---
+++ readme.md
@@ -0,0 +1,78 @@
+## Создание нового HTML -тега
+</hr>
+
+-document.createElement(Tag):
+
+```js
+
+let title = document.createElement('div') 
+//создает новый html элемент 'div'
+
+title.className = 'title'
+// присвоение класса элементу
+
+title.innerHTML = 'This is our first news.'
+
+//Присоение элементу содержимого
+
+console.log(title)
+
+// <div class = "title">This is our first title</div>
+
+document.body.append(title);
+
+// добавление элемента на страницу
+
+```
+## Первый параметр element.insertAdjacentHtml
+
+
+специальное слово, указывающее, куда по отношению к elem производить вставку. 
+
+
+###Значение должно быть одним из следующих:
+
+- "beforebegin" – вставить html непосредственно перед elem,
+- "afterbegin" – вставить html в начало elem,
+- "beforeend" – вставить html в конец elem,
+- "afterend" – вставить html непосредственно после elem.
+
+```js
+<div id="about"></div>
+<script>
+  about.insertAdjacentHTML('beforebegin', '<h1>Welcome to our dental clinik</h1>');
+  about.insertAdjacentHTML('afterbegin', '<p>We provide all necessary procedures for the health of your teeth ahnd oral cavity</p>');
+</script>
+
+// Результат в html:
+// <h1>Welcome to our dental clinik</h1>
+//<div id ="about"><p>We provide all necessary procedures for the health of //your teeth ahnd oral cavity</p></div>
+
+
+```
+
+## Как удалить элемент из DOM-дерева:
+
+</hr>
+
+Element.remove() - мы можем удалить любой элемент из DOM-дерева
+
+```js
+
+<h1>Welcome to our dental clinik</h1>
+<div id ="about"><p>We provide all necessary procedures for the health of //your teeth ahnd oral cavity</p></div>
+
+<script>
+
+const paragraph = document.querySelector("p");
+
+paragraph.remove();
+
+
+</script>
+
+// Результат в  html:
+// <h1>Welcome to our dental clinik</h1>
+//<div id ="about"></div>
+
+```
